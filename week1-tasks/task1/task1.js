bee0c4f0d3350d80b4b77980c436d33725714be9
Ira <ira.kumpula@metropolia.fi>
2025-10-24 12:42:27 +0300

    add task1 temperature converter

---
+++ week1-tasks/task1/task1.js
@@ -0,0 +1,16 @@
+/*
+Task 1
+
+Write a program that prompts the user for a temperature in Celsius and converts it to Fahrenheit and Kelvin. The program should then display the converted temperatures in an HTML document.
+Calculate the temperature in Fahrenheit by using the formula: F = (C * 9/5) + 32, where F is the temperature in Fahrenheit and C is the temperature in Celsius.
+
+2p
+*/
+
+const degreesCelsius = prompt('Give temperature (C): ');
+
+const degreesFahrenheit = (degreesCelsius * 9) / 5 + 32;
+
+const outputElement = document.querySelector('main');
+
+outputElement.innerText = `${degreesCelsius} Celsius is ${degreesFahrenheit} Fahrenheit. `;
