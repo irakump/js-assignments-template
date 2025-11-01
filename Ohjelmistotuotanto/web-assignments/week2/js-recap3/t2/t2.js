375527b95334bf6797794fec96a1d76032d88113
Ira <ira.kumpula@metropolia.fi>
2025-11-01 15:28:46 +0200

    Add Task 2 - Todo list with DOM methods

--- Ohjelmistotuotanto/web-assignments/week2/js-recap3/t2/t2.js
+++ Ohjelmistotuotanto/web-assignments/week2/js-recap3/t2/t2.js
@@ -1,3 +1,10 @@
+/*
+Task 2 - Todo list with DOM methods
+The assignment remains unchanged from the first, except that for this version, DOM methods are used to add the todo items.
+for attribute is added to <label> with htmlFor
+4p
+*/
+
 // array for todo list
 const todoList = [
   {
@@ -27,4 +34,37 @@ const todoList = [
   },
 ];
 
-// add your code here
+function updateList(list) {
+  const ulElement = document.querySelector('ul');
+
+  for (let item of list) {
+    const id = `todo-${item.id}`;
+
+    // create elements
+    const li = document.createElement('li');
+    const input = document.createElement('input');
+    const label = document.createElement('label');
+
+    // set input attributes
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('id', id);
+
+    // check whether item is checked (completed is true)
+    if (item.completed) {
+      input.setAttribute('checked', '');
+    }
+
+    // label content
+    label.htmlFor = `${id}`;
+    label.innerHTML = item.task;
+
+    // append elements
+    li.appendChild(input);
+    li.appendChild(label);
+    ulElement.appendChild(li);
+
+  }
+
+}
+
+updateList(todoList);
