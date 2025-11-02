1e53ef2e48d1e9fafadaf07c8d752ad6617dc988
Ira <ira.kumpula@metropolia.fi>
2025-11-02 21:09:14 +0200

    feat: fetch user data - log response

---
+++ Ohjelmistotuotanto/web-assignments/week2/js-recap5/t1/t1.js
@@ -0,0 +1,24 @@
+/*
+Task 1 - GET Method
+Practice using the Fetch API with async/await to make a GET request and log the response.
+Utilize the Fetch API with async/await to make a GET request to the URL https://reqres.in/api/users/1.
+Log the response data to the console.
+*/
+
+async function getData() {
+  try {
+    const response = await fetch('https://reqres.in/api/users/1', {
+      headers: {
+      'x-api-key': 'reqres-free-v1',
+    },
+    });
+
+    const jsonData = await response.json();
+    console.log(jsonData);
+
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+getData();
